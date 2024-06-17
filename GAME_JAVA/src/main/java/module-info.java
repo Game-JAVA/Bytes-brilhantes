7171bac8ac75ee26454cca7bc01273944a1db49f
gabriel faria <gabrielfariasilva10@hotmail.com>
2024-06-17 16:47:00 -0300

    feat: class image created

--- GAME_JAVA/src/main/java/module-info.java
+++ GAME_JAVA/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.game_java {
 
     requires org.controlsfx.controls;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens game to javafx.fxml;
     exports game;
