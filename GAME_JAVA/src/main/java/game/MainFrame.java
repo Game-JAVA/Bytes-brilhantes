8f8132266de5811678c052bd9a8af4583a1b86f8
BrunoSakamoto <brunosakar2005@gmail.com>
2024-06-24 17:24:04 -0300

    feat: Adding enemyClass

--- GAME_JAVA/src/main/java/game/MainFrame.java
+++ GAME_JAVA/src/main/java/game/MainFrame.java
@@ -199,18 +199,33 @@ public class MainFrame extends JFrame implements Runnable {
         bottomDivision.setRightComponent(buttonsPanel);
 
         //Ação de cada botão - A IMPLEMENTAR!!
+
+        //Botão de ataque
         buttons.getFirst().addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 int attack = heroes.get(currentHero).attack(heroes.get(1));
-                texts.setText(heroes.getFirst().getName() + " attacked " + heroes.get(1).getName() + ", dealing " + attack + " damage!");
+                texts.setText(heroes.get(currentHero).getName() + " attacked " + heroes.get(1).getName() + ", dealing " + attack + " damage!");
             }
         });
-        buttons.get(3).addActionListener(new ActionListener() {
+
+        //Ação do Botão de defesa
+        buttons.get(1).addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                currentHero++;
-                System.out.println(currentHero);
+                heroes.get(currentHero).defend();
+                texts.setText(heroes.get(currentHero).getName() + " defended herself from " + heroes.get(1).getName());
             }
         });
+
+        // Ação do Botão de poder especial
+
+        buttons.get(2).addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                heroes.get(currentHero).specialPower(heroes.get(1));
+                texts.setText(heroes.getFirst().getName() + " especial" + heroes.get(1).getName());
+            }
+        });
+
+
 
         //Instancia as barras de vida iniciais (começando com 10 de vida)
         Image heroHealth = new Image("img/Health/10.png", 50, 170);
