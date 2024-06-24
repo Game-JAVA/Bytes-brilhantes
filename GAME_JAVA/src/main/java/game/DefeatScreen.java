240921d6511b027d438ddd8a1463cd83aa7938ca
gabriel faria <gabrielfariasilva10@hotmail.com>
2024-06-24 13:49:57 -0300

    feat: MainScreen added, along with attack functionality, healthbars implemented, hero and enemy, implemented, swap functionality started

--- GAME_JAVA/src/main/java/game/DefeatScreen.java
+++ GAME_JAVA/src/main/java/game/DefeatScreen.java
@@ -10,7 +10,7 @@ public class DefeatScreen{
 
     DefeatScreen() {
         f = new JFrame("Derrota"); // Cria um JFrame com o título "Derrota"
-        img = new ImageIcon("src/main/java/game/Tela de Derrota.gif"); // Carrega a imagem GIF
+        img = new ImageIcon("img/Tela de Derrota.gif"); // Carrega a imagem GIF
 
         l = new JLabel("", img, JLabel.CENTER); // Cria um JLabel com a imagem centralizada
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Fecha o programa ao fechar a janela
--- GAME_JAVA/src/main/java/module-info.java
+++ GAME_JAVA/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.example.game_java {
     requires org.controlsfx.controls;
     requires com.almasb.fxgl.all;
     requires java.desktop;
+    requires java.logging;
 
     opens game to javafx.fxml;
     exports game;
