3a9a2cf61821602adfb4d8cf6263fd19d07e8107
valentinaoliveira <voli.leite05@outlook.com>
2024-07-02 13:37:12 -0300

    feat: ending cofiguration from EnemyClass + swapping hero after death

---
+++ GAME_JAVA/src/main/java/game/Boss.java
@@ -0,0 +1,15 @@
+package game;
+
+
+public class Boss extends Character{
+
+    public Boss(int health, int attack, int defense, int powerLoad, String name, String url, int position_x, int position_y, String sound) {
+        super(health, attack, defense, powerLoad, name, url, position_x, position_y, sound);
+    }
+    @Override
+    public void specialPower( Character c){
+        //Definir poder especial
+        this.setPowerCharge(0);
+    }
+}
+
